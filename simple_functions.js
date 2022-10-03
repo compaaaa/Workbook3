4420b394bf80bc7e9efc1736a2b25ffbfc8fb6e0
compaaaa <azi.varela2003@gmail.com>
2022-10-03 13:19:08 -0400

    displayReceipt

---
+++ simple_functions.js
@@ -0,0 +1,21 @@
+//DEFINE THREE NAMED FUNCTION
+function favoriteThings(){
+    console.log("Haziel");
+    console.log("Inception");
+    console.log("Zillakami");
+}
+
+
+function whyImHere(){
+    console.log("I joined this program because I heard it was a good alternative to college and could get me paid a lot.");
+}
+
+
+function favoritePlace(){
+    console.log("My favorite place is Florida, I like the cities that are near beaches like Sarasota.");
+}
+
+//CALL ALL THREE FUNCTIONS BY NAME
+favoriteThings();
+whyImHere();
+favoritePlace();
